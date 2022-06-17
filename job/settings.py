90ba7378426787a83eab0723ff7d2481cf742f76
YMirodilbek <mirodilbek.backend@gmail.com>
2022-06-17 09:21:44 +0500

    nes in settings

--- job/settings.py
+++ job/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-y+%*u-y#fmi5_!#c(_+v+r_4&2(u^+d#ra*j7n47rez2z2cnkz'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
