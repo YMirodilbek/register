507eaabacf6ae350202a0d994a4b6f51bdabc975
YMirodilbek <mirodilbek.backend@gmail.com>
2022-07-14 08:09:56 +0500

    deleteItem

--- job/settings.py
+++ job/settings.py
@@ -119,7 +119,6 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATICFILES_DIRS=[BASE_DIR / 'static']
-STATIC_ROOT = 'C:\Users\User\Desktop\system'
 MEDIA_URL = 'media/'
 MEDIA_ROOT = BASE_DIR / 'media'
 
